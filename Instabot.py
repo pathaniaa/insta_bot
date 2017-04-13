034233c9e4141dc0f184e0053340a42f6274f198
pathaniaa <childhudstories@gmail.com>
2017-04-13 10:37:29 +0530

    Update Instabot.py

--- Instabot.py
+++ Instabot.py
@@ -1,6 +1,6 @@
 ''' This is instabot can do various things such as :
     1 --> Check your Profile
-    2 --> like a post (one at)
+    2 --> like a post (one)
     3 --> comment on a post
     4 --> delete a comment as well as  multiple comments by searching comments by words
     5 --> Find Average number of words per comment on a post
